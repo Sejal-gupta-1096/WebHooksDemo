6cebc0864be0469cd5be85aa7ebccd6e8051ac54
Sejal <sejal1096.cse18@chitkara.edu.in>
2020-11-13 14:13:21 +0530

    commit agter adding webhook

--- WebHooksDemo/main.py
+++ WebHooksDemo/main.py
@@ -8,9 +8,10 @@ app = FastAPI()
 def index():
     return "Hii guyz"
 
+
 @app.post("/github")
 def api_github_response(request:Request):
-    print(request.json())
+    print(request);
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
