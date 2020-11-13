923abbab21841ee33c400a5e5ecbed6a196a4222
Sejal <sejal1096.cse18@chitkara.edu.in>
2020-11-13 13:51:24 +0530

    Commit 1

---
+++ WebHooksDemo/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI,Request
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return "Hii guyz"
+
+@app.post("/github")
+def api_github_response(request:Request):
+    print(request.json())
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
