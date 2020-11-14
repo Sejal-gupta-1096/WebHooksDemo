c781bd6856d92f1adfdef6e121cd8ac5cb4a1bc5
Sejal <sejal1096.cse18@chitkara.edu.in>
2020-11-14 12:49:56 +0530

    commit

--- WebHooksDemo/main.py
+++ WebHooksDemo/main.py
@@ -12,7 +12,7 @@ def index():
 
 @app.post("/github")
 def api_github_response(request:Request):
-    print(request.json());
+    print(request.json())
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
