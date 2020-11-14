52997ceea10c66573583100833bffd09d10321ba
Sejal <sejal1096.cse18@chitkara.edu.in>
2020-11-14 14:28:19 +0530

    commit

--- WebHooksDemo/main.py
+++ WebHooksDemo/main.py
@@ -12,7 +12,7 @@ def index():
 
 @app.post("/github")
 async def api_github_response(request:Request):
-    json_data = json.loads(str(request.body, encoding='utf-8'))
+    json_data = json.loads(request.body.decode(encoding='UTF-8'))
     print(json_data)
 
 if __name__ == "__main__":
